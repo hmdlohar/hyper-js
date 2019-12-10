3aa0e48b0ec018512ebc2487ec2232c33938ab3d
hamid <hmdlohar@gmail.com>
2019-12-10 20:25:28 +0530

    some

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const api = require('./Services/Api')
 const utils = require('./Services/Utils')
+const lsu = require('./Services/LocalStorageUtils')
+const auth = require('./Services/AuthServices')
 
 module.exports ={
     api,
     utils,
+    lsu,
+    auth
 }
